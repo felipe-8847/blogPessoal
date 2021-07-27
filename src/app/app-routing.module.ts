c6d8b2908864aa1fb9482106bef31feaf754999d
felipe ferreira <felipealves131351@gmail.com>
2021-07-27 09:24:25 -0300

    rotas de login e cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+  {path:'entrar',component: EntrarComponent},
+  {path:'cadastrar',component: CadastrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
